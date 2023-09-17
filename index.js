698006e0b46b6a08b0fafa21533dc2599e3b1068
atp <atp.aung.5555@gmail.com>
2023-09-17 15:52:42 +0630

    c

--- index.js
+++ index.js
@@ -38,15 +38,15 @@ app.get("/info", (request, response) => {
     response.send(`<p>phone book has ${personCount} people</p><p>${date}</p>`)
 })
 
-// app.get('/api/notes/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     const note = notes.find(note => note.id === id)
-//     if (note) {
-//         response.json(note)
-//     } else {
-//         response.status(404).end()
-//     }
-// })
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(note => note.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
